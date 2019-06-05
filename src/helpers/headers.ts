f91dc694241d2968898d9bfb7d4d4886774f5533
BigBug <mengdong@nancal.com>
2019-06-05 18:11:45 +0800

    feat(/src/index.ts add helper/data.ts): 添加处理u 与 headers的函数，并且增加了一些工具方法rl的函数
    对url做处理，当用户发送的数据 为 对象 数字符串 都做了相应的处理
    
    BREAKING CHANGE: no

---
+++ src/helpers/headers.ts
@@ -0,0 +1,24 @@
+import { isPlainObject } from './util'
+
+function normalizeHeaderName(headers: any, normalizedName: string): void {
+  if (!headers) {
+    return
+  }
+  Object.keys(headers).forEach(name => {
+    if (name !== normalizedName && name.toUpperCase() === normalizedName.toUpperCase()) {
+      headers[normalizedName] = headers[name]
+      delete headers[name]
+    }
+  })
+}
+
+// data参数的作用是判断发送到服务端的数据是不是一个对象json数据，是一个json数据时才做处理
+export function processHeaders(headers: any, data: any): any {
+  normalizeHeaderName(headers, 'Content-Type')
+
+  if (isPlainObject(data)) {
+    if (headers && !headers['Content-Type']) {
+      headers['Content-Type'] = 'application/json;/chartset=utf-8'
+    }
+  }
+}
---
+++ src/helpers/util.ts
@@ -0,0 +1,14 @@
+const toString = Object.prototype.toString
+
+export function isDate(val: any): val is Date {
+  return toString.call(val) === '[object Date]'
+}
+
+export function isObject(val: any): val is Object {
+  return val !== null && typeof val === 'object'
+}
+
+// 精准判断是否是一个普通对象
+export function isPlainObject(val: any): val is Object {
+  return toString.call(val) === '[object Object]'
+}
